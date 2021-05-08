11a166d5a92151daf6bcfb281908a53dd04d40b5
Superted441 <Superted441X@gmail.com>
2021-05-08 19:37:10 +0100

    Basic Operations button finnished

--- Tkinter_Calc.py
+++ Tkinter_Calc.py
@@ -2,6 +2,7 @@ from tkinter import *
 #from numpy import *
 
 
+
 root = Tk()
 
 
@@ -11,6 +12,7 @@ def basicbuttons(): # Basic Operations window
 
     def Quitb():
         Basic.destroy()
+        basicbuttons()
 
     global x
     x = 0
@@ -19,9 +21,11 @@ def basicbuttons(): # Basic Operations window
     global z
     z = 0
     global x1
-    x1 = False
+    x1 = True
     global decimal
     decimal = False
+    global d1
+    d1 = False
 
     bf1 = Frame(Basic) # bf1 is for text
     bf2 = Frame(Basic) # other frames are for button placment
@@ -45,14 +49,16 @@ def basicbuttons(): # Basic Operations window
         Textbox1.delete("1.0","end")
 
     def ONE():
+        global d1
         if x1: # if on num 1
             global y
             y = str(y)
             if decimal == False: #and not a decimal
-                y.replace(".0", "") # remove the point to continue as whole number
+                y = y.replace(".0", "") # remove the point to continue as whole number
             else:
+
                 if d1 == True:
-                    y.replace([-1], "") # remove the Zero
+                    y = y.replace(".0", ".") # remove the Zero
                     d1 = False
                 else:
                     pass
@@ -61,36 +67,418 @@ def basicbuttons(): # Basic Operations window
         else: # no2
             global x
             x = str(x)
-            if decimal == False:
-                x.replace(".0", "")
+            if decimal == False: #and not a decimal
+                x = x.replace(".0", "") # remove the point to continue as whole number
             else:
-                pass
+
+                if d1 == True:
+                    x = x.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
             x = x  + "1"
             x = float(x)
+        clearTextInput()
+        Textbox1.insert(INSERT, "{0} {2} {1}".format(y, x, z))
+        Textbox1.pack()
+
+
+    def TWO():
+        global d1
+        if x1: # if on num 1
+            global y
+            y = str(y)
+            if decimal == False: #and not a decimal
+                y = y.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    y = y.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            y = y + "2"
+            y = float(y)
+        else: # no2
+            global x
+            x = str(x)
+            if decimal == False: #and not a decimal
+                x = x.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    x = x.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            x = x  + "2"
+            x = float(x)
+        clearTextInput()
+        Textbox1.insert(INSERT, "{0} {2} {1}".format(y, x, z))
+        Textbox1.pack()
+
+
+    def THREE():
+        global d1
+        if x1: # if on num 1
+            global y
+            y = str(y)
+            if decimal == False: #and not a decimal
+                y = y.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    y = y.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            y = y + "3"
+            y = float(y)
+        else: # no2
+            global x
+            x = str(x)
+            if decimal == False: #and not a decimal
+                x = x.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    x = x.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            x = x  + "3"
+            x = float(x)
+        clearTextInput()
+        Textbox1.insert(INSERT, "{0} {2} {1}".format(y, x, z))
+        Textbox1.pack()
+
+
+    def FOUR():
+        global d1
+        if x1: # if on num 1
+            global y
+            y = str(y)
+            if decimal == False: #and not a decimal
+                y = y.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    y = y.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            y = y + "4"
+            y = float(y)
+        else: # no2
+            global x
+            x = str(x)
+            if decimal == False: #and not a decimal
+                x = x.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    x = x.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            x = x  + "4"
+            x = float(x)
+        clearTextInput()
+        Textbox1.insert(INSERT, "{0} {2} {1}".format(y, x, z))
+        Textbox1.pack()
+
+
+    def FIVE():
+        global d1
+        if x1: # if on num 1
+            global y
+            y = str(y)
+            if decimal == False: #and not a decimal
+                y = y.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    y = y.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            y = y + "5"
+            y = float(y)
+        else: # no2
+            global x
+            x = str(x)
+            if decimal == False: #and not a decimal
+                x = x.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    x = x.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            x = x  + "5"
+            x = float(x)
+        clearTextInput()
+        Textbox1.insert(INSERT, "{0} {2} {1}".format(y, x, z))
+        Textbox1.pack()
+
+
+    def SIX():
+        global d1
+        if x1: # if on num 1
+            global y
+            y = str(y)
+            if decimal == False: #and not a decimal
+                y = y.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    y = y.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            y = y + "6"
+            y = float(y)
+        else: # no2
+            global x
+            x = str(x)
+            if decimal == False: #and not a decimal
+                x = x.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    x = x.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            x = x  + "6"
+            x = float(x)
+        clearTextInput()
+        Textbox1.insert(INSERT, "{0} {2} {1}".format(y, x, z))
+        Textbox1.pack()
+
+    def SEVEN():
+        global d1
+        if x1: # if on num 1
+            global y
+            y = str(y)
+            if decimal == False: #and not a decimal
+                y = y.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    y = y.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            y = y + "7"
+            y = float(y)
+        else: # no2
+            global x
+            x = str(x)
+            if decimal == False: #and not a decimal
+                x = x.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    x = x.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+                x = x  + "7"
+                x = float(x)
+        clearTextInput()
+        Textbox1.insert(INSERT, "{0} {2} {1}".format(y, x, z))
+        Textbox1.pack()
+
+
+    def EIGHT():
+        global d1
+        if x1: # if on num 1
+            global y
+            y = str(y)
+            if decimal == False: #and not a decimal
+                y = y.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    y = y.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            y = y + "8"
+            y = float(y)
+        else: # no2
+            global x
+            x = str(x)
+            if decimal == False: #and not a decimal
+                x = x.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    x = x.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            x = x  + "8"
+            x = float(x)
+        clearTextInput()
+        Textbox1.insert(INSERT, "{0} {2} {1}".format(y, x, z))
+        Textbox1.pack()
+
+    def NINE():
+        global d1
+        if x1: # if on num 1
+            global y
+            y = str(y)
+            if decimal == False: #and not a decimal
+                y = y.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    y = y.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            y = y + "9"
+            y = float(y)
+        else: # no2
+            global x
+            x = str(x)
+            if decimal == False: #and not a decimal
+                x = x.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    x = x.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            x = x  + "9"
+            x = float(x)
+        clearTextInput()
+        Textbox1.insert(INSERT, "{0} {2} {1}".format(y, x, z))
+        Textbox1.pack()
+
+    def ZERO():
+        global d1
+        h = 0
+        i = 0
+        if x1: # if on num 1
+            global y
+            y = str(y)
+            if decimal == False: #and not a decimal
+                y = y.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    y = y.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            y = y + "0"
+            h = float(y)
+        else: # no2
+            global x
+            x = str(x)
+            if decimal == False: #and not a decimal
+                x = x.replace(".0", "") # remove the point to continue as whole number
+            else:
+
+                if d1 == True:
+                    x = x.replace(".0", ".") # remove the Zero
+                    d1 = False
+                else:
+                    pass
+            x = x  + "0"
+            i = float(x)
+        clearTextInput()
+        Textbox1.insert(INSERT, "{0} {1} {2}".format(h, z, i))
+        Textbox1.pack()
+
+
+    def POINT():
+        global decimal
+        global d1
+        decimal = True
+        d1 = True
+
+    def PLUS():
+        global decimal
+        global x1
+        global z
+        global function
+        z = "+"
+        x1 = False
+        decimal = False
+        function = "add"
+
+    def MINUS():
+        global decimal
+        global x1
+        global z
+        global function
+        z = "-"
+        x1 = False
+        decimal = False
+        function = "minus"
+
+
+    def TIMES():
+        global decimal
+        global x1
+        global z
+        global function
+        z = "x"
+        x1 = False
+        decimal = False
+        function = "times"
+
+    def DIVIDE():
+        global decimal
+        global x1
+        global z
+        global function
+        z = "/"
+        x1 = False
+        decimal = False
+        function = "divide"
+
+    def EQUALS():
+        try:
+            if function == "divide":
+                k = y / x
+            elif function == "add":
+                k = y + a
+            elif function == "minus":
+                k = y - x
+            elif function == "times":
+                k= y * x
+            else:
+                pass
             clearTextInput()
-            Textbox1.insert(INSERT, "{0} {1} {2}".format(x, z, y))
+            Textbox1.insert(INSERT, "{}".format(k))
             Textbox1.pack()
-
-
-
+        except:
+            pass
 
     One = Button(bf4, text = "1", command = ONE, height = 2, width = 8)
-    Two = Button(bf4, text = "2", command = apass, height = 2, width = 8)
-    Three = Button(bf4, text = "3", command = apass, height = 2, width = 8)
-    Four = Button(bf3, text = "4", command = apass, height = 2, width = 8)
-    Five = Button(bf3, text = "5", command = apass, height = 2, width = 8)
-    Six = Button(bf3, text = "6", command = apass, height = 2, width = 8)
-    Seven = Button(bf2, text = "7", command = apass, height = 2, width = 8)
-    Eight = Button(bf2, text = "8", command = apass, height = 2, width = 8)
-    Nine = Button(bf2, text = "9", command = apass, height = 2, width = 8)
-    Zero = Button(bf5, text = "0", command = apass, height = 2, width = 8)
-    Times = Button(bf2, text = "X", command = apass, height = 2, width = 8)
-    Divide = Button(bf3, text = "/", command = apass, height = 2, width = 8)
-    Add = Button(bf4, text = "+", command = apass, height = 2, width = 8)
-    Point = Button(bf5, text = ".", command = apass, height = 2, width = 8)
-    Minus = Button(bf5, text = "-", command = apass, height = 2, width = 8)
-    Equals = Button(bf5, text = "=", command = apass, height = 2, width = 8)
-    GoBack = Button(bf1, text = "Go Back", command = Quitb, height = 2, width = 8)
+    Two = Button(bf4, text = "2", command = TWO, height = 2, width = 8)
+    Three = Button(bf4, text = "3", command = THREE, height = 2, width = 8)
+    Four = Button(bf3, text = "4", command = FOUR, height = 2, width = 8)
+    Five = Button(bf3, text = "5", command = FIVE, height = 2, width = 8)
+    Six = Button(bf3, text = "6", command = SIX, height = 2, width = 8)
+    Seven = Button(bf2, text = "7", command = SEVEN, height = 2, width = 8)
+    Eight = Button(bf2, text = "8", command = EIGHT, height = 2, width = 8)
+    Nine = Button(bf2, text = "9", command = NINE, height = 2, width = 8)
+    Zero = Button(bf5, text = "0", command = ZERO, height = 2, width = 8)
+    Times = Button(bf2, text = "X", command = TIMES, height = 2, width = 8)
+    Divide = Button(bf3, text = "/", command = DIVIDE, height = 2, width = 8)
+    Add = Button(bf4, text = "+", command = PLUS, height = 2, width = 8)
+    Point = Button(bf5, text = ".", command = POINT, height = 2, width = 8)
+    Minus = Button(bf5, text = "-", command = MINUS, height = 2, width = 8)
+    Equals = Button(bf5, text = "=", command = EQUALS, height = 2, width = 8)
+    GoBack = Button(bf1, text = "Reset", command = Quitb, height = 2, width = 8)
 
 
     One.pack(side = LEFT)
@@ -114,7 +502,8 @@ def basicbuttons(): # Basic Operations window
 
     Basic.mainloop()
 
-def ops():
+"""
+def ops(): Middle menu that was removed due to my laziness
     opschoice = Tk()
 
 
@@ -127,12 +516,12 @@ def ops():
 
     button2 = Button(frameops, text = "Buttons", command = basicbuttons, height = 3, width = 20)
     button3 = Button(frameops, text = "Go Back", command = opsquit, height = 3, width = 10)
-    button1.pack(side = LEFT)
+
     button3.pack(side = LEFT)
     button2.pack(side = LEFT)
 
     opschoice.mainloop()
-
+"""
 
 def Quitmainc(): # quit feature for quit button
     root.destroy()
@@ -154,7 +543,7 @@ Textbox.insert(INSERT, "Welcome Please make a Choice") # welcoming text
 Textbox.pack()
 
 
-Choice1 = Button(frame1, text = "Basic Operations", command = ops, height = 3, width = 20) # buttons to access different features
+Choice1 = Button(frame1, text = "Basic Operations", command = basicbuttons, height = 3, width = 20) # buttons to access different features
 Choice2 = Button(frame1, text = "X to the Power of Y", command = apass, height = 3, width = 20)
 Choice3 = Button(frame1, text = "Circle Operations", command = apass, height = 3, width = 20)
 Choice4 = Button(frame1, text = "The Square Root", command = apass, height = 3, width = 20)
